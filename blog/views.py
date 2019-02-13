19bc91cfe5b31f3d41a442a304c55f15be763aad
Mohamed Bouchi <mohamedsubhibouchi@gmail.com>
2019-02-13 22:51:17 +0100

    import bootstrap, create base template, add dummy data, add main css.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'author1',
+        'title': 'title1',
+        'content': 'content1',
+        'date_posted': 'august 1, 2018'
+    },
+    {
+        'author': 'author2',
+        'title': 'title2',
+        'content': 'content2',
+        'date_posted': 'august 2, 2018'
+    },
+    {
+        'author': 'author3',
+        'title': 'title3',
+        'content': 'content3',
+        'date_posted': 'august 3, 2018'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts,
+        'title': 'home'
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'about'
+    }
+    return render(request, 'blog/about.html')
