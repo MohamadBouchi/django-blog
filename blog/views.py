301d26f3d9ef48b73808127625e9e1f44bba0564
Mohamed Bouchi <mohamedsubhibouchi@gmail.com>
2019-02-14 21:13:42 +0100

    Post models created with dummy data and migrated and registerd to the admin site

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'author1',
-        'title': 'title1',
-        'content': 'content1',
-        'date_posted': 'august 1, 2018'
-    },
-    {
-        'author': 'author2',
-        'title': 'title2',
-        'content': 'content2',
-        'date_posted': 'august 2, 2018'
-    },
-    {
-        'author': 'author3',
-        'title': 'title3',
-        'content': 'content3',
-        'date_posted': 'august 3, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'home'
     }
     return render(request, 'blog/home.html', context)
